afcdaf923fc7b5d62e267ab0108365094824fb56
ZurekTomasz <a@a.pl>
2017-04-26 01:39:28 +0200

    1.0.1

--- CsvFiles/CsvFiles.cpp
+++ CsvFiles/CsvFiles.cpp
@@ -56,16 +56,16 @@ void Files::overwrite()
 	std::ofstream file_save;
 	file_save.open(file_name.c_str());
 
+	file_save << "1000,1,0,100,100\n";
+	file_save << "Ola,ma,kota,100,100\n";
+	file_save << "Ola ma kota,1,0,30000,300\n";
+	file_save << "4000,1,0,500,500\n";
+
+	/*
 	file_save << "1000;1;0;100;100\n";
 	file_save << "2000;1;0;30000;300\n";
 	file_save << "3000;1;0;500;500\n";
 	file_save << "4000;1;0;700;700\n";
-
-	/*
-	file_save << "1000,1,0,100,100\n";
-	file_save << "2000,1,0,30000,300\n";
-	file_save << "3000,1,0,500,500\n";
-	file_save << "4000,1,0,700,700\n";
 	*/
 
 	file_save.close();
